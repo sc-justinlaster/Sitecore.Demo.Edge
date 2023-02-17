ee7654328e4c28d11acbbfaa96fc64ce69e283aa
sc-justinlaster <102692267+sc-justinlaster@users.noreply.github.com>
2023-02-17 16:55:21 -0500

    Initialize feagture/8484-send-embed-form-component

---
+++ Website/src/rendering/src/components/Forms/EmbedSendForm.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { LayoutServicePageState, useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
+
+type EmbedSendFormProps = {
+  params: { [key: string]: string };
+};
+
+const EmbedSendForm = (props: EmbedSendFormProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext.pageState === LayoutServicePageState.Edit;
+  if (!props?.params?.sendFormId) {
+    if (!isPageEditing) {
+      return <></>;
+    }
+
+    return (
+      <section className="section">
+        <div className="section-content col-content container">
+          <p className="section-content-p">
+            <strong>Action required</strong> - Sitecore Send Form Component
+          </p>
+          <p>Add the form ID as a rendering parameter on this component</p>
+        </div>
+      </section>
+    );
+  }
+
+  if (isPageEditing) {
+    return (
+      <section className="section">
+        <div className="section-content col-content container">
+          <p className="section-content-p">Sitecore Send Form Component</p>
+          <p>Switch to preview mode to view the form</p>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="section">
+      <div data-mooform-id={`${props?.params?.sendFormId}`} />
+    </section>
+  );
+};
+
+export default EmbedSendForm;
